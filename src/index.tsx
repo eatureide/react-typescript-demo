71bc666c2daccc436a41e72d5f2d080416544577
Eature <781746820@qq.com>
2019-12-01 23:05:23 +0800

    1

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Hello from './constants/Hello'
+import { Provider } from 'react-redux'
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { enthusiasm } from './reducers'
+
+const store = createStore(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <Hello />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
